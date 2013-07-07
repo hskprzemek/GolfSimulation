f09c64a632d3ae32027b94d568e6b5510b94d301
hskprzemek <hskprzemek@gmail.com>
2013-07-07 21:15:33 +0200

    New stats

--- golf-domain/src/main/java/com/hsk/gapp/model/Stats.java
+++ golf-domain/src/main/java/com/hsk/gapp/model/Stats.java
@@ -7,4 +7,36 @@ public class Stats {
 	private int power;
 	private int shortgame;
 
+	public int getPutting() {
+		return putting;
+	}
+
+	public void setPutting(int putting) {
+		this.putting = putting;
+	}
+
+	public int getAccuracy() {
+		return accuracy;
+	}
+
+	public void setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public int getShortgame() {
+		return shortgame;
+	}
+
+	public void setShortgame(int shortgame) {
+		this.shortgame = shortgame;
+	}
+
 }
